33d0d741d6d0c23f1261c6aecec4e17269a3ffb2
DmitryYakin <93880312+DmitryYakin@users.noreply.github.com>
2025-08-17 23:28:47 +0300

    Learn about a types of Functions

--- functions.js
+++ functions.js
@@ -1,6 +1,13 @@
-function sayHi(name, abs, afdaw){
-    console.log('Hi! ' + name)
-    console.log(abs+afdaw-18*5+12)
+// function sayHi(name, abs, afdaw){
+//     console.log('Hi! ' + name)
+//     console.log(abs+afdaw-18*5+12)
+// }
+
+// sayHi('Dima', 2, 3)
+
+function multu(num1,num2)
+{
+    const a = num1*num2;
 }
 
-sayHi('Dima', 2, 3)
+console.log(multu(2,3))
