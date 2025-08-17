ea2e6c6ad24383fa2b046f9a72dbfdb31a14e962
DmitryYakin <93880312+DmitryYakin@users.noreply.github.com>
2025-08-17 22:58:29 +0300

    Learned about Objects
    Learning objects

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # LearnJS
 Learning new program language
+
+# Lear about varuables in JS
+Learned about:
+ - how to declare and initialize varuables;
+ - what is const;
+ - types of varuables.
+
+ # Dynamic programming
+ 
---
+++ objects.js
@@ -0,0 +1,15 @@
+let course = {
+    name: 'JS for beg',
+    hours: 3
+};
+// console.log(course.name);
+
+// course.name = 'suck';
+
+// console.log(course.name);
+course['name'] = 'pomenyal';
+console.log(course['name']);
+
+
+let property = 'hours';
+console.log(course[property]);
