0ed91847e621a2de9f882a2a6fa4904991edeb92
DmitryYakin <93880312+DmitryYakin@users.noreply.github.com>
2025-08-17 15:33:07 +0300

    Variables
    learned about variables

---
+++ variables.js
@@ -0,0 +1,19 @@
+let COLOR_GREEN = 'green';
+let one = "one";
+let string3 = `JS`;
+
+let otwo = 2;
+let pu = 3.14;
+let verylarge = 1234123412412412n;
+
+let awda = true;
+
+let favcolor;
+console.log (favcolor);
+
+const uniqueKey = Symbol();
+
+let course = {
+    name: 'JS for BEG',
+    hours: 3
+};
