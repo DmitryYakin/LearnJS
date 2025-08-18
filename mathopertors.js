c71605116bb60a5f5ff45c67636a651137d5debd
DmitryYakin <93880312+DmitryYakin@users.noreply.github.com>
2025-08-18 22:46:55 +0300

    Update operators

---
+++ mathopertors.js
@@ -0,0 +1,27 @@
+// let num1 = 14
+// let num2 = 12 
+// const res = num1 <= num2
+// console.log(res)
+
+// let a = 2;
+// let b = '2';
+
+// console.log(a==b)
+
+// console.log(a===b)
+
+// console.log (true == 1)
+
+// let age = 25;
+// const canDrink = age >=18 ? true : false
+// console.log(canDrink)
+
+// || - OR && - AND ! - NOT  ?? - to assigned a default value for variable if it's not define (null or undefiend)
+//  Example of ??
+
+let a = null;
+//a = 4
+const res = a ?? 3; // (a !== null && a !== undefined) ? a : 3;
+
+
+console.log(a, res)
